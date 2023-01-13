d365cb693950d0fe92433a46cebd2784e05d8f52
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2023-01-13 12:42:24 +0600

    Add banner and benefits blocks for mobile apps resolution

--- src/templates/data.js
+++ src/templates/data.js
@@ -6,4 +6,36 @@ module.exports = {
     'WHOLESALE',
     'CONTACT',
   ],
+  banner: {
+    label: 'Good4Me',
+    title: [
+      'MULTI-VITAMIN',
+      '(3 MONTH SUPPLY)',
+    ],
+    description: 'Good4Me Apple Cider Vinegar gummies are the newest addition to your morning health and well-being regime.',
+    button: 'Shop Now',
+  },
+  benefits: {
+    items: [
+      'GLUTEN FREE',
+      'VEGAN FRIENDLY',
+      'NO NASTIES',
+      'PECTIN BASE',
+    ],
+    title: 'We Have The Main Process to Run The GOOD4ME',
+    cards: [
+      {
+        name: 'Quality Assured',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.",
+      },
+      {
+        name: 'Quality Assured',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.",
+      },
+      {
+        name: 'Quality Assured',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.",
+      },
+    ]
+  }
 };
