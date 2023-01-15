996a944c5d49131433aed991be1d34466d36670d
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2023-01-15 23:03:21 +0600

    Add about company block for mobile apps resolution and rename images

--- src/templates/data.js
+++ src/templates/data.js
@@ -22,7 +22,10 @@ module.exports = {
       'NO NASTIES',
       'PECTIN BASE',
     ],
-    title: 'We Have The Main Process to Run The GOOD4ME',
+    title: [
+      'We Have The Main Process to',
+      'Run The GOOD4ME',
+    ],
     cards: [
       {
         name: 'Quality Assured',
@@ -36,6 +39,36 @@ module.exports = {
         name: 'Quality Assured',
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.",
       },
-    ]
-  }
+    ],
+  },
+  about: {
+    title: [
+      'TAKE',
+      'CONTROL OF',
+      'YOUR HEALTH',
+    ],
+    facts: [
+      {
+        title: 'It is a long established fact that a reader.',
+        description: 'It is a long established fact that a reader will be distracted by the...',
+      },
+      {
+        title: 'This a long established fact that.',
+        description: 'It is a long established fact that a reader will be distracted by the...',
+      },
+      {
+        title: 'We a long established fact that as.',
+        description: 'It is a long established fact that a reader will be distracted by the...',
+      },
+      {
+        title: 'This a long established fact that long.',
+        description: 'It is a long established fact that a reader will be distracted by the...',
+      },
+    ],
+    info: [
+      'The Good4Me range has been formulated based on scientific. Our vitamins are here and ready to boost your mood, immunity and overall well-being!',
+      'Our vitamins are here and ready to boost your mood. New Zealand from local and imported ingredients.',
+    ],
+    button: 'BROWSE OUR RANGE',
+  },
 };
